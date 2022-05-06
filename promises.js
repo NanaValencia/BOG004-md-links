bcb564976398039bf711197fb52d249852705f7c
NanaValencia <diseno.1988@gmail.com>
2022-05-05 19:16:42 -0500

    realicé función de mdLinks para validar links y crear objeto close #11

--- promises.js
+++ promises.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const { resolve } = require('path');
 const path = require('path');
-const mdLinkExtractor = require('markdown-link-extractor');
 const linkCheck = require('link-check');
+// const file = require('link-check/lib/proto/file');
+const markdownLinkExtractor = require('markdown-link-extractor');
 
 //Esta constante guarda la ruta que se ingresa en consola
 const userPath = process.argv[2];
@@ -21,7 +22,7 @@ const pathValidation = (route) => {
 
 //Identifica si el archivo es .md
 const identifyFile = (userPath) => {
-    const mdFile = path.extname(pathValidation(userPath)) === 'md';
+    const mdFile = path.extname(pathValidation(userPath)) === '.md';
     return mdFile;
 };
 
@@ -29,8 +30,9 @@ const identifyFile = (userPath) => {
 const readNewFile = (userPath) => {
     return new Promise ((resolve, reject) => {
         fs.readFile(userPath, 'UTF-8', (error, file) => {
-            if (error){
-                throw error;
+            if (error) {
+                reject('soy un error de leer')
+                throw error;    
             }
             if (!identifyFile(userPath)) {
                 reject('No se puede leer el archivo');
@@ -41,17 +43,67 @@ const readNewFile = (userPath) => {
     });
 };
 
-// readNewFile(userPath).then ((file)=>{
-//     const { links } = mdLinkExtractor(file,extended = true);
-//     const basicInfoLinks = [];
-  
-//     links.forEach((link) => {
-//         const basicInfoLink = {};
-//         basicInfoLink.href = link.href;
-//         basicInfoLink.text = link.text;
-//         basicInfoLink.file = userPath;
-//         basicInfoLinks.push(basicInfoLink);
-//         console.log(basicInfoLinks);
-//     })
-// });
+const validateState = (objectArray) => {
+    return new Promise ((resolve, reject) => {
+        const objectLink = objectArray.href;
+        linkCheck(objectLink, (error, result) => {
+if (error) {
+    console.log(error, 'SOY ERROR');
+    return;
+}
+let statusResponse = '';
+if (result.status === 'alive') {
+    statusResponse = 'ok';
+} else {
+    statusResponse = 'fail';
+}
+resolve ({file: objectArray.file, href: objectArray.href, statusCode: result.statusCode, status: statusResponse, text: objectArray.text});
+        })
+    })
+}
+const mdLinks = (path, options) => {
+    return new Promise((resolve, reject) => {
+        //Ingresa path
+        //Función para convertir la ruta en absoluta
+        const absoluteRoute = pathValidation(userPath);
+        //Función que evalua si la ruta es un archivo .md
+        identifyFile(absoluteRoute);
+        //Función que lee el archivo y crea el objeto
+        const basicInfoLinks = [];
+        readNewFile(absoluteRoute)
+        .then((file) =>{
+            //Se crea una constante para usar la libreria markdownLinkExtractor, para extraer los links
+            const { links } = markdownLinkExtractor(file,(extended = true));
+            const arrayLinks = links.map((link) => {
+            let linksObject = {};
+            linksObject.file = userPath,
+            linksObject.href = link.href,
+            linksObject.text = link.text,
+            basicInfoLinks.push(linksObject);
+            return linksObject;
+        });
+        // resolve(basicInfoLinks)
+        return basicInfoLinks;
+         })
+    .then((res) => {
+        if(options === ''){
+            res.map((e) => {
+                resolve(`${e.file} ${e.href} ${e.text}`)
+            })
+        }else {
+            resolve(Promise.all(res.map((e) => validateState(e))))
+        }  
+    })
+    .catch((error) => {console.log(error);
+        reject('Hubo un problema con la ejecución')
+      });
+    });
+}
 
+mdLinks(userPath)
+.then((res) => {
+    console.log(res, 'Esta es la función validar')
+})
+.catch ((err) => {
+    console.log(err, 'cayó en error')
+});
