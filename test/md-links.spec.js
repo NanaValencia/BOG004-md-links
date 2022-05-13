012be17f92f9736883a1f6183c32cdc854305a25
NanaValencia <diseno.1988@gmail.com>
2022-05-13 15:28:10 -0500

    se crearon test para las funciones validar ruta, leer archivo y validar links

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,53 @@
-const mdLinks = require('../');
+const {readNewFile} = require('../index.js')
+const {pathValidation} = require('../index.js')
+const {identifyFile} = require('../index.js')
+const {validateLink} = require('../index.js')
+const linkCheck = require('link-check')
+
+jest.mock('link-check');
+
+describe('pathValidation', () => {
+  it('debería retornar una ruta absoluta', () =>{
+    expect(pathValidation('./prueba.md')).toEqual('C:\\Users\\ASUS\\Desktop\\LABORATORIA\\BOG004-md-links\\prueba.md')
+  })
+});
+
+describe ('Prueba de lectura de archivo', () => {
+  it('Función read resuelta', async () => {
+    await expect(readNewFile('C:\\Users\\ASUS\\Desktop\\LABORATORIA\\BOG004-md-links\\test\\prueba.md')).resolves.toBe('Este es un archivo de prueba');
+  })
+});
 
 
-describe('mdLinks', () => {
+const objectTest =
+{
+  file: './testFile',
+  href: 'http://es.wikipedia.org/wiki/Markdown',
+  text: 'Markdown'
+}
+const objectResolve =
+{
+  file: './testFile',
+  href: 'http://es.wikipedia.org/wiki/Markdown',
+  text: 'Markdown',
+  statusCode: 200,
+  status: 'ok'
+}
 
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
+describe('Prueba para validar el estado de los links', () => {
+  const linkCheck = require('link-check')
+  it('validateLink', () => 
+    validateLink(objectTest)
+    .then((objectResolve) => {
+      expect(objectResolve.statusCode).toBe(200);
+    })
+    .catch((err) => console.log(err, ('Está cayendo en el error')))
 
+  );
+
+describe('prueba para validar el estado de los links', () => {
+  const linkCheck = require('link-check')
+  it('validateLink', async () => {
+    await expect(validateLink(objectTest)).resolves.toEqual(objectResolve)})
+})
 });
